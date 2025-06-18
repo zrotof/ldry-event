a685118bdced97c9a983d8888f377bb3393e848c
Samuel Mandeng <zrotof@Air-de-Samuel.lan>
2025-06-18 15:09:55 +0200

    updates

---
+++ dist/dj-ldry/browser/chunk-UBES2Y5R.js
@@ -0,0 +1,1 @@
+import{Ia as n,ab as o,bb as r,qb as a}from"./chunk-B3NKUI6U.js";var c=class t{static \u0275fac=function(e){return new(e||t)};static \u0275cmp=n({type:t,selectors:[["app-feature-products-container"]],decls:2,vars:0,template:function(e,i){e&1&&(o(0,"p"),a(1,"feature-products-container works!"),r())},encapsulation:2})};export{c as FeatureProductsContainerComponent};
